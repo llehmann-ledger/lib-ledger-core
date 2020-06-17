1c04fef3663c7d0a1905e650241ce84c92299a1b
llehmann-ledger <65655263+llehmann-ledger@users.noreply.github.com>
2020-06-17 14:17:57 +0200

    Add stellar Migrations files

---
+++ ledger-core-stellar/inc/stellar/database/Migrations.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <core/api/DatabaseBackendType.hpp>
+#include <core/database/Migrations.hpp>
+
+//#include <stellar/StellarLikeCoinID.hpp> FIXME
+
+namespace ledger {
+    namespace core {
+        /// Tag type.
+        struct StellarMigration {
+          //static int constexpr COIN_ID = STELLAR_COIN_ID; FIXME
+          //static uint32_t constexpr CURRENT_VERSION = 3; FIXME
+        };
+
+        //TODO
+  }
+}
---
+++ ledger-core-stellar/src/stellar/database/Migrations.cpp
@@ -0,0 +1,10 @@
+#include <stellar/database/Migrations.hpp>
+
+namespace ledger {
+    namespace core {
+        int constexpr StellarMigration::COIN_ID;
+        uint32_t constexpr StellarMigration::CURRENT_VERSION;
+
+        //TODO
+    }
+}
