52e78d3329367d49035af28130597e203e4e51b8
llehmann-ledger <65655263+llehmann-ledger@users.noreply.github.com>
2020-06-19 13:39:39 +0200

    Add StellarLikeCoinID

--- ledger-core-stellar/inc/stellar/database/Migrations.hpp
+++ ledger-core-stellar/inc/stellar/database/Migrations.hpp
@@ -3,14 +3,14 @@
 #include <core/api/DatabaseBackendType.hpp>
 #include <core/database/Migrations.hpp>
 
-//#include <stellar/StellarLikeCoinID.hpp> FIXME
+#include <stellar/StellarLikeCoinID.hpp>
 
 namespace ledger {
     namespace core {
         /// Tag type.
         struct StellarMigration {
-          //static int constexpr COIN_ID = STELLAR_COIN_ID; FIXME
-          //static uint32_t constexpr CURRENT_VERSION = 3; FIXME
+          static int constexpr COIN_ID = STELLAR_COIN_ID;
+          static uint32_t constexpr CURRENT_VERSION = 1;
         };
 
         //TODO
