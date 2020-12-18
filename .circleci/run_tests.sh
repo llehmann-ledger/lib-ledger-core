e2ea116dbfa1ae7ed162cf736b7f7b790f515b64
llehmann-ledger <65655263+llehmann-ledger@users.noreply.github.com>
2020-12-18 16:11:03 +0100

    Make CI run tests in parallel

--- .circleci/run_tests.sh
+++ .circleci/run_tests.sh
@@ -3,9 +3,9 @@ export MOCK_HTTP_FOLDER=`pwd`/../lib-ledger-core/core/test/fixtures/mocked_http/
 echo "mocked tests:"
 ls $MOCK_HTTP_FOLDER  
 if [ "$1" == "linux" ]; then
-    ~/cmake-3.16.5-Linux-x86_64/bin/ctest -VV -timeout 120
+    ~/cmake-3.16.5-Linux-x86_64/bin/ctest -j4 --output-on-failure --timeout 120
 else
-    ctest -VV -timeout 120
+    ctest -j4 --output-on-failure
 fi
 
 
